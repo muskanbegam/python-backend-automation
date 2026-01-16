3e9e0d453361b315da1700bd9077fe223d09b897
muskanbegam <mkmuskanbegam@gmail.com>
2026-01-16 23:03:21 +0530

    Third commit

---
+++ file_rename.py
@@ -0,0 +1,32 @@
+import os
+
+# Path of the folder containing files
+folder_path = "files"
+
+# Get list of all items in the folder.
+files = os.listdir(folder_path)
+
+count = 1
+
+for filename in files:
+
+    
+    old_path = os.path.join(folder_path, filename)
+
+    # Skip if it's not a file
+    if not os.path.isfile(old_path):
+        continue
+
+    # Split filename and extension
+    name, extension = os.path.splitext(filename)
+
+    # Create new filename
+    new_name = f"file_{count}{extension}"
+    new_path = os.path.join(folder_path, new_name)
+
+    # Rename the file
+    os.rename(old_path, new_path)
+
+    count += 1
+
+print("Batch rename completed successfully.")
---
+++ guess.py
@@ -0,0 +1,26 @@
+import random
+
+def guess_number(guess,num):
+        if guess == num:
+                print("You guessed it Right!!")
+                return False
+        else:
+                print("oh no its a wrong guess")
+                return True
+
+
+print("Let's play the Guessing Game")
+print("****************************")
+num = random.randint(1,11)
+is_play=True
+trials=0
+
+while is_play==True:
+    trials += 1
+    guess = int(input("Enter your guess from 1 to 11: "))
+    is_play= guess_number(guess,num)
+
+print(f"Guessed in {trials} times.")
+
+
+
